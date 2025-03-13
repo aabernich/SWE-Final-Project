e5ccdc55031028d3b371b040976f06cf747f6bc7
aabernich <aabernich@gmail.com>
2025-03-13 17:59:22 -0400

    Added backend start with some database integration

---
+++ backend/database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE internationalFabrics;
+USE internationalFabrics;
+
+CREATE TABLE users (
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    passwordHash VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (username, email, passwordHash)
+VALUES ('john doe', 'johndoe@gmail.com', 'testPassword');
